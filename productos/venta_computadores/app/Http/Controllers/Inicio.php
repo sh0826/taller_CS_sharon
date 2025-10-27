edf7a1c103bb65f1e2ccba9d55780443831dbccd
SHARON HERNANDEZ <sharon.dp.hernandez@gmail.com>
2025-10-27 16:11:50 -0500

    add

--- productos/venta_computadores/app/Http/Controllers/Inicio.php
+++ productos/venta_computadores/app/Http/Controllers/Inicio.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class inicioSesionControlador extends Controller
+class Inicio extends Controller
 
 {
     // Muestra el formulario de login
