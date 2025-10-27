1651e6e6c0e584761db74ce119eb027f5e2eeb60
SHARON HERNANDEZ <sharon.dp.hernandez@gmail.com>
2025-10-27 16:03:17 -0500

    eliminar

--- productos/venta_computadores/app/Models/Producto.php
+++ productos/venta_computadores/app/Models/Producto.php
@@ -1,31 +1,10 @@
 <?php
 
-/**
- * Created by Reliese Model.
- */
 
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * Class Producto
- * 
- * @property int $id_pc
- * @property string $nombre
- * @property string $marca
- * @property string|null $modelo
- * @property string|null $procesador
- * @property string|null $ram
- * @property string|null $almacenamiento
- * @property float $precio
- * @property string|null $imagen
- * @property int $tipo_producto_id
- * 
- * @property TipoProducto $tipo_producto
- *
- * @package App\Models
- */
 class Producto extends Model
 {
 	protected $table = 'producto';
