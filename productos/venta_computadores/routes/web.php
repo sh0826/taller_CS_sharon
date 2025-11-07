30489bd694364f86d7ca4b1bdfcca040d7dca3fb
SHARON HERNANDEZ <sharon.dp.hernandez@gmail.com>
2025-11-06 23:06:48 -0500

    add

--- productos/venta_computadores/routes/web.php
+++ productos/venta_computadores/routes/web.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\inicioSesionControlador;
+use App\Http\Controllers\Inicio;
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\TipoProductoController;
 
-Route::get('/', [inicioSesionControlador::class, 'showLoginForm'])->name('inicio');
-Route::post('/login', [inicioSesionControlador::class, 'login'])->name('login');
+Route::get('/', [Inicio::class, 'showLoginForm'])->name('inicio');
+Route::post('/login', [Inicio::class, 'login'])->name('login');
 
 Route::resource('productos', ProductoController::class);
 Route::resource('tipo_productos', TipoProductoController::class);
